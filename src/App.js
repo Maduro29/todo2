c020a10fd119dd8d673d17e86a08936d3cd1e11b
Maduro29 <manhdungtran.vnuuet@gmail.com>
2023-04-27 22:17:55 +0700

    add data

--- src/App.js
+++ src/App.js
@@ -1,31 +1,40 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  let items = [
+    {
+      id: 1,
+      text: 'Take out the trash',
+      completed: false
+    },
+    {
+      id: 2,
+      text: 'Join viewing party',
+      completed: false
+    },
+    {
+      id: 3,
+      text: 'Play basketball',
+      completed: false
+    }
+  ]
+
   return (
     <div className="container">
       <div className="row">
         <div className="todolist">
           <h1>Todos</h1>
           <ul className="list-group list-group-flush">
-            <li className="list-group-item">
-              <div className="form-check">
-                <input className="form-check-input" type="checkbox" value="" id="todo-item-check-1" />
-                <label className="form-check-label" htmlFor="todo-item-check-1">Take out the trash</label>
-              </div>
-            </li>
-            <li className="list-group-item">
-              <div className="form-check">
-                <input className="form-check-input" type="checkbox" value="" id="todo-item-check-2" />
-                <label className="form-check-label" htmlFor="todo-item-check-2">Buy bread</label>
-              </div>
-            </li>
-            <li className="list-group-item">
-              <div className="form-check">
-                <input className="form-check-input" type="checkbox" value="" id="todo-item-check-3" />
-                <label className="form-check-label" htmlFor="todo-item-check-3">Teach penguins to fly</label>
-              </div>
-            </li>
+            {items.map(item =>
+              <li className="list-group-item">
+                <div className="form-check">
+                  <input className="form-check-input" type="checkbox" value="" id={`todo-item-check-${item.id}`} />
+                  <label className="form-check-label" htmlFor={`todo-item-check-${item.id}`}>
+                    {item.text}
+                  </label>
+                </div>
+              </li>
+            )}
           </ul>
         </div>
       </div>
